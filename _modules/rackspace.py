af3d00e09ec3e9484adc005ba24484ca5ee5e6b1
ozgurakan <ozgurakan@gmail.com>
2014-05-22 04:37:58 +0000

    POC for images list

--- _modules/rackspace.py
+++ _modules/rackspace.py
@@ -7,7 +7,7 @@ import logging
 import urllib2
 import random
 import time
-
+import sys
 import requests
 import json
 
@@ -36,14 +36,42 @@ def init(username, api_key, account_id=None):
     return token
 '''
 
-def init(username, api_key):
+def init(username, api_key, account_id):
+    headers = connect(username, api_key)
+    return get_images(headers, account_id)
+    
+
+def connect(username, api_key):
     url = 'https://identity.api.rackspacecloud.com/v2.0/tokens'
     payload  = {"auth":{"RAX-KSKEY:apiKeyCredentials":{"username": username , "apiKey": api_key }}}
     headers = {'Content-Type': 'application/json'}
     r = requests.post(url, data=json.dumps(payload), headers=headers)
     token = r.json()['access']['token']['id']
-    headers = {'X-Auth-Token' : token, 'Content-Type': 'application/json', 'Client-ID': 'QClient1'}
-    return token
+    headers = {'X-Auth-Token' : token}
+    return headers
+
+def get(url, headers, payload=None):
+    r = requests.get(url, data=json.dumps(payload), headers=headers)
+    return [r.status_code, r.headers, r.content]
+
+def post(url, headers, payload=None):
+    r = requests.post(url, data=json.dumps(payload), headers=headers)
+    return [r.status_code, r.headers, r.content]
+
+def put(url, headers, payload=None):
+    r = requests.put(url, data=json.dumps(payload), headers=headers)
+    return [r.status_code, r.headers, r.content]
+
+def delete(url, headers, payload=None):
+    r = requests.delete(url, data=json.dumps(payload), headers=headers)
+    return [r.status_code, r.headers, r.content]
+
+
+def get_images(headers, account_id):
+    url = 'https://iad.servers.api.rackspacecloud.com/v2/' + str(account_id) + '/flavors'
+    return get(url, headers)
     
 
 
+if __name__ == "__main__":
+    init(sys.argv[1], sys.argv[2], sys.argv[3])
